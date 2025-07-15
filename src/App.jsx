24ece595619131ab1d10a236a35b9646db16c61c
Galih Adhi Kusuma <84978361+NoxturneDev@users.noreply.github.com>
2025-07-15 08:28:44 +0700

    init project and demo

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Gallery3DViewer from "./3d/Gallery3DViewer"
+import GalleryViewer from "./2d/GalleryViewer"
+
+export default function App() {
+  return (
+    <GalleryViewer />
+    // <Gallery3DViewer/>
+  )
+}
